5165b5ff1b138c4fb7e13058d4ad9e63032bb0bb
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-07-16 08:30:34 +1200

    Ensure that new tokens will be received on presentation pages
    Co-Authored-By: Zach Mandeville <zz@ii.coop>

--- static/prezzie-init.js
+++ static/prezzie-init.js
@@ -1,6 +1,6 @@
 import "/socket.io/socket.io.js"
 import "/qrcode.min.js"
-const sessionID = localStorage.getItem("sessionID")
+const monitor = localStorage.getItem("sessionID")
 const {socketID, secret} = JSON.parse(localStorage.getItem("token") || '{}')
 console.log("INIT ITNIT", {socketID, secret})
 
@@ -21,19 +21,33 @@ function renderQRCode (val) {
 
 document.addEventListener("DOMContentLoaded", () => {
   const URL = window.location.origin
-  const socket = io(URL, {autoConnect: false});
+  const socket = io(URL, {autoConnect: true});
 
-  renderQRCode(`${window.location.origin}/presentations.html?m=${sessionID}`);
+  renderQRCode(`${window.location.origin}/presentations.html?m=${monitor}`);
 
   socket.on("set presentation", ({ presentation, sessionID }) => {
-    const monitor = socket.auth.sessionID
-    if (sessionID !== monitor) {
+    console.log("setting presentation to", {presentation, sessionID})
+    if (monitor !== sessionID) {
       return
     } else {
-      prezzyLink = `${window.location.origin}/presentations/${presentation}/presentation_client.html`
+      const prezzyLink = `${window.location.origin}/presentations/${presentation}/presentation_client.html`
       window.location.href = prezzyLink;
     }
   });
+  socket.on("new token", ({ sessionID, socketID, secret }) => {
+    console.log({ sessionID, socketID, secret })
+    if (sessionID !== monitor) {
+      console.log("no match", {monitor, sessionID})
+      return
+    } else {
+      console.log("match!", {monitor, sessionID})
+      localStorage.setItem("token", JSON.stringify({ socketID }))
+      window.socketID = socketID
+    }
+
+    console.log("what happening", socket.auth)
+  })
+
 })
 
 window.socketID = socketID
