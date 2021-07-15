fb3ba6962728ca8a94d84f7a7e127b923fbbec7d
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-07-15 14:45:58 +1200

    Add presentation script content
    Co-Authored-By: Zach Mandeville <zz@ii.coop>

--- static/prezzie-init.js
+++ static/prezzie-init.js
@@ -1,1 +1,40 @@
-const {socketID, secret} = localStorage.get("token");
+import "/socket.io/socket.io.js"
+import "/qrcode.min.js"
+const sessionID = localStorage.getItem("sessionID")
+const {socketID, secret} = JSON.parse(localStorage.getItem("token") || '{}')
+console.log("INIT ITNIT", {socketID, secret})
+
+const slideBody = document.querySelector("div.reveal")
+var qrcode = document.createElement("canvas")
+qrcode.id = 'qrcode-canvas'
+slideBody.appendChild(qrcode)
+
+function renderQRCode (val) {
+    QRCode.toCanvas(qrcode, val, {width: 135, height: 135}, (error) => {
+    if (error) {
+      console.log('error rendering qr code', {error, value: val});
+    } else {
+      console.log('qr code success!', {value: val});
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const URL = window.location.origin
+  const socket = io(URL, {autoConnect: false});
+
+  renderQRCode(`${window.location.origin}/presentations.html?m=${sessionID}`);
+
+  socket.on("set presentation", ({ presentation, sessionID }) => {
+    const monitor = socket.auth.sessionID
+    if (sessionID !== monitor) {
+      return
+    } else {
+      prezzyLink = `${window.location.origin}/presentations/${presentation}/presentation_client.html`
+      window.location.href = prezzyLink;
+    }
+  });
+})
+
+window.socketID = socketID
+window.secret = secret
