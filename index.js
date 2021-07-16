b5ba1ddc1af95ff67ceb64041682628a994428ea
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-07-16 16:07:26 +1200

    Rethink implementation
    Co-Authored-By: Zach Mandeville <zz@ii.coop>

--- index.js
+++ index.js
@@ -16,6 +16,49 @@ const io = require("socket.io")(server,{
   }
 });
 
+const monitorState = {
+  monitorID: '',
+  monitorName: '',
+  presentation: '',
+  controllerID: '',
+  token: '',
+  secret: '',
+  dateOfBirth:'',
+  lastUpdated:'',
+}
+
+let sotw = [];
+
+session = sotw;
+
+// find session
+// session.find(item => item.monitorID === '')
+
+async function newMonitor () {
+  const date = new Date();
+  const pokemons =  await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}&offset=${randomInLimit()}`);
+  const pokemonData = await pokemons.json();
+  const pokemon = pokemonData.results[randomInLimit()];
+  return {
+    ...monitorState,
+    monitorID: randomID(),
+    monitorName: pokemon.name,
+    dateOfBirth: date,
+    lastUpdated: date
+  }
+}
+
+
+function updateMonitor (id, newState) {
+  return sotw.map(m => {
+    return m.monitorID === id
+      ? {...m, ...newState}
+    : m
+  })
+}
+
+// sotw = updateMonitor(id, newState)
+
 var connected = 0
 
 const opts = {
@@ -30,8 +73,8 @@ const randomInLimit = () =>
       Math.floor(Math.random() * (LIMIT - 1));
 
 const createHash = secret => {
-	let cipher = crypto.createCipher('blowfish', secret);
-	return cipher.final('hex');
+  let cipher = crypto.createCipher('blowfish', secret);
+  return cipher.final('hex');
 };
 
 function getDirectories(path) {
@@ -39,6 +82,10 @@ function getDirectories(path) {
     .readdirSync(path)
     .filter((file) => fs.statSync(`${path}/${file}`).isDirectory());
 }
+
+// function findSession (monitorName) {
+//   return monitorState.filter(obj => )
+// }
 
 function saveSesh (sessionID, vals) {
   const session = sessionStore.findSession(sessionID)
@@ -58,45 +105,49 @@ function getPresentations() {
 
 app.use(express.static(opts.baseDir));
 
-io.use((socket, next) => {
-  const sessionID = socket.handshake.auth.sessionID;
-  if (sessionID) {
-    console.log("found session id: ", sessionID)
-    const session = sessionStore.findSession(sessionID);
-    if (session) {
-      console.log("found session: ");
-      socket.sessionID = sessionID;
-      socket.isMonitor = session.isMonitor;
-      socket.monitorName = session.monitorName;
-      return next();
-    }
-  }
-  socket.sessionID = randomID();
-  socket.isMonitor = false;
-  console.log("new session:", socket.sessionID)
-  next();
-});
+// io.use((socket, next) => {
+//   const monitor = socket.handshake.auth.monitor;
+//   if (monitor) {
+//     console.log("found monitor: ", monitor.monitorID)
+
+//     const session = sessionStore.findSession(sessionID);
+//     if (session) {
+//       console.log("found session: ");
+//       socket.sessionID = sessionID;
+//       socket.isMonitor = session.isMonitor;
+//       socket.monitorName = session.monitorName;
+//       return next();
+//     }
+//   }
+//   socket.sessionID = randomID();
+//   socket.isMonitor = false;
+//   console.log("new session:", socket.sessionID)
+//   next();
+// });
 
 io.on("connection", (socket) => {
   connected += 1
-  console.log(`[${connected}] session ${socket.sessionID} connected`);
-  sessionStore.saveSession(socket.sessionID, {
-    userID: socket.sessionID,
-    isMonitor: socket.isMonitor || false,
-    monitorName: socket.monitorName || '',
-    presentation: socket.Presentation || '',
-    connected: true,
-  });
-  socket.emit("session", {
-    sessionID: socket.sessionID,
-    isMonitor: socket.isMonitor,
-    monitorName: socket.monitorName
-  });
+  if (socket.handshake.auth.monitor) {
+    console.log(`[${connected}] session ${socket.handshake.auth.monitor.monitorID} connected`);
+  } else {
+    console.log(`[${connected}] client session ${socket.id} connected`);
+  }
+  newState = updateMonitor(socket.handshake.auth.monitor);
+  sotw = newState
+
+  socket.emit("connection initialized", socket.handshake.auth.monitor);
 
   socket.on("disconnect", () => {
     connected -= 1
     console.log(`[${connected}] user disconnected`);
   });
+
+  socket.on("new monitor", async () => {
+    let monitor = await newMonitor();
+    console.log({monitor});
+    sotw = [...sotw, monitor];
+    socket.emit('monitor added', monitor);
+  })
 
   socket.on("monitor set", async () => {
     console.log("monitor set")
@@ -105,6 +156,7 @@ io.on("connection", (socket) => {
     const pokemonData = await pokemons.json();
     const pokemon = pokemonData.results[randomInLimit()];
     saveSesh(socket.sessionID, {isMonitor: true, monitorName: pokemon.name});
+
     io.emit("monitor go!", { monitorName: pokemon.name, sessionID: socket.sessionID });
   });
 
@@ -124,17 +176,23 @@ io.on("connection", (socket) => {
       console.log("No session ID found when obtaining token")
       return
     }
-  	const ts = new Date().getTime();
-  	const rand = Math.floor(Math.random()*9999999);
-  	const secret = ts.toString() + rand.toString();
+    const ts = new Date().getTime();
+    const rand = Math.floor(Math.random()*9999999);
+    const secret = ts.toString() + rand.toString();
     const socketID = createHash(secret)
     console.log(`New token generated ${socketID}`)
     saveSesh(sessionID, { socketID, secret })
-  	socket.emit("token for you", { sessionID, socketID, secret });
-  	io.emit("new token", { sessionID, socketID, secret });
+    socket.emit("token for you", { sessionID, socketID, secret });
+    io.emit("new token", { sessionID, socketID, secret });
   })
   io.emit("hello", { name: "Zach", adjective: "cool" });
 });
 
 server.listen(opts.port || null);
 console.log(`Listening on ${opts.port} and serving ${opts.baseDir}`);
+
+// socket.on('add monitor', () => {
+//   monitor = newMonitor();
+//   sotw = [...sotw, monitor]
+//   io.emit('monitor added', monitor);
+// })
