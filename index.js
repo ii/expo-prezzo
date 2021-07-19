e59a1cd6ac09698e82f759511ee404a100747bca
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-07-19 14:25:09 +1200

    refactor for more functional design
    Co-Authored-By: zachmandeville <zachmandeville@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,79 +1,37 @@
 const http = require("http");
 const express = require("express");
 const fs = require("fs");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const crypto = require("crypto");
-const { InMemorySessionStore } = require('./session-store');
-
-const LIMIT = 300;
-
-const app = express();
-const server = http.createServer(app);
-const sessionStore = new InMemorySessionStore();
-const io = require("socket.io")(server,{
-  cors: {
-    origin: "https://node.bobymcbobs.pair.sharing.io/"
-  }
-});
-
-const monitorState = {
-  monitorID: '',
-  monitorName: '',
-  presentation: '',
-  controllerID: '',
-  token: '',
-  secret: '',
-  dateOfBirth:'',
-  lastUpdated:'',
-}
-
-let sotw = [];
-
-session = sotw;
-
-// find session
-// session.find(item => item.monitorID === '')
-
-async function newMonitor () {
-  const date = new Date();
-  const pokemons =  await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}&offset=${randomInLimit()}`);
-  const pokemonData = await pokemons.json();
-  const pokemon = pokemonData.results[randomInLimit()];
-  return {
-    ...monitorState,
-    monitorID: randomID(),
-    monitorName: pokemon.name,
-    dateOfBirth: date,
-    lastUpdated: date
-  }
-}
-
-// MonitorState => MonitorState
-// return monitor with token and secret added
-function updateToken (monitor) {
-  const ts = new Date().getTime();
-  // we are using reveal's encryption logic to make sure our tokens work with it.
-  const rand = Math.floor(Math.random()*9999999);
-  const secret = ts.toString() + rand.toString();
-  const token = createHash(secret)
-  return {...monitor, token, secret};
-}
-
-// monitorID, MonitorState => SOTW
-function updateMonitor (id, newState) {
-  return sotw.map(m => {
-    return m.monitorID === id
-      ? {...m, ...newState}
-    : m
-  })
-}
-
-// sotw = updateMonitor(id, newState)
 
 const opts = {
   port: process.env.PORT || 8101,
   baseDir: process.env.BASEDIR || process.cwd() + "/static",
+  url: process.env.URL || "https://node.bobymcbobs.pair.sharing.io/"
 };
+
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: opts.url
+  },
+});
+
+const monitorState = {
+  monitorID: "",
+  monitorName: "",
+  presentation: "",
+  controllerID: "",
+  token: "",
+  secret: "",
+  dateOfBirth: "",
+  lastUpdated: "",
+};
+
+let sotw = []; // the state of the app, will hold a list of MonitorStates;
+
+const LIMIT = 300;
 
 const randomID = () =>
       crypto.randomBytes(8).toString("hex");
@@ -81,119 +39,122 @@ const randomID = () =>
 const randomInLimit = () =>
       Math.floor(Math.random() * (LIMIT - 1));
 
-const createHash = secret => {
-  let cipher = crypto.createCipher('blowfish', secret);
-  return cipher.final('hex');
-};
+// String => Hex
+// encodes secret, s, into cryptographic hash
+// we are using a deprecated, less secure method, but our threat model is v. low.
+const createHash = (s) =>
+  crypto.createCipher("blowfish", s).final("hex");
 
-function getDirectories(path) {
-  return fs
-    .readdirSync(path)
-    .filter((file) => fs.statSync(`${path}/${file}`).isDirectory());
+const getDirectories = (path) => (
+  fs.readdirSync(path)
+    .filter((file) => fs.statSync(`${path}/${file}`).isDirectory()));
+
+async function newMonitor() {
+  const date = new Date();
+  const pokemons = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}&offset=${randomInLimit()}`
+  );
+  const pokemonData = await pokemons.json();
+  const pokemon = pokemonData.results[randomInLimit()];
+  return {
+    ...monitorState,
+    monitorID: randomID(),
+    monitorName: pokemon.name,
+    dateOfBirth: date,
+    lastUpdated: date,
+  };
 }
 
-// function findSession (monitorName) {
-//   return monitorState.filter(obj => )
-// }
-
-function saveSesh (sessionID, vals) {
-  const session = sessionStore.findSession(sessionID)
-  if (session) {
-    sessionStore.saveSession(sessionID, {
-      ...session,
-      ...vals
-    });
-  } else {
-    console.log("couldn't find session");
+// => Token
+// generate new token/secret pair
+function updateToken() {
+  const ts = new Date().getTime();
+  // we are using reveal's encryption logic to make sure our tokens work with it.
+  const rand = Math.floor(Math.random() * 9999999);
+  const secret = ts.toString() + rand.toString();
+  return {
+    secret,
+    token: createHash(secret)
   }
 }
 
-function getPresentations() {
-  return getDirectories(opts.baseDir + "/presentations/");
+// string, monitorID, MonitorState => SOTW
+// given a match type and matching value,
+// update  matching monitor in SOTW with new MonitorState
+function updateMonitorInSOTW(match, value, newState) {
+  return sotw.map((m) => {
+    return m[match] === value
+      ? { ...m, ...newState }
+      : m;
+  });
 }
 
 app.use(express.static(opts.baseDir));
 
-// io.use((socket, next) => {
-//   const monitor = socket.handshake.auth.monitor;
-//   if (monitor) {
-//     console.log("found monitor: ", monitor.monitorID)
-
-//     const session = sessionStore.findSession(sessionID);
-//     if (session) {
-//       console.log("found session: ");
-//       socket.sessionID = sessionID;
-//       socket.isMonitor = session.isMonitor;
-//       socket.monitorName = session.monitorName;
-//       return next();
-//     }
-//   }
-//   socket.sessionID = randomID();
-//   socket.isMonitor = false;
-//   console.log("new session:", socket.sessionID)
-//   next();
-// });
-
 io.on("connection", (socket) => {
   if (socket.handshake.auth.monitor) {
-    console.log(`[${sotw.length+1}] session ${socket.handshake.auth.monitor.monitorID} connected`);
+    console.log(`[${sotw.length + 1}] monitor ${socket.handshake.auth.monitor.monitorName} connected`);
+    // when developing, often the server restarts but the monitor sessions remain.
+    // in these cases, the monitor will have an id and a name, but won't exist in the sotw.
+    // here we check that and add it to the sotw
+    const monitorInSOTW = sotw.find(m => m.monitorID === socket.handshake.auth.monitor.monitorID);
+    if (!monitorInSOTW) {
+      sotw = [...sotw, socket.handshake.auth.monitor];
+    }
   } else {
-    console.log(`[${sotw.length+1}] client session ${socket.id} connected`);
+    console.log(`[${sotw.length + 1}] client session ${socket.id} connected`);
   }
-  newState = updateMonitor(socket.handshake.auth.monitor);
-  sotw = newState
 
   socket.emit("connection initialized", socket.handshake.auth.monitor);
 
   socket.on("disconnect", () => {
     let monitor = socket.handshake.auth.monitor;
     if (monitor) {
-      sotw = sotw.filter(m => m.monitorID != monitor.monitorID);
-      console.log(`[${sotw.length}] Monitor ${monitor.monitorName} disconnected`)
-      console.log({sotw})
+      sotw = sotw.filter((m) => m.monitorID != monitor.monitorID);
+      console.log(
+        `[${sotw.length}] Monitor ${monitor.monitorName} disconnected`
+      );
     } else {
-      console.log(`[${sotw.length-1}] user disconnected`);
+      console.log(`[${sotw.length - 1}] user disconnected`);
     }
   });
 
   socket.on("new monitor", async () => {
     let monitor = await newMonitor();
     sotw = [...sotw, monitor];
-    socket.emit('monitor added', monitor);
-  })
+    socket.emit("monitor added", monitor);
+  });
 
-  socket.on("get token", (monitorName) => {
-    console.log('get token request')
-    let monitor = sotw.find(m => m.monitorName === monitorName);
-    console.log({monitor, sotw, monitorName});
+  socket.on("new token requested", (monitorName) => {
+    const monitor = sotw.find((m) => m.monitorName === monitorName);
+    const { token, secret } = updateToken(monitor);
     if (typeof monitor === "undefined" || !monitor) {
-      console.log("client trying to sync without a monitor")
-      return
+      console.log("client trying to sync without a monitor", {monitorName, sotw});
+      return;
+    } else {
+      console.log("new token requested");
+      sotw = updateMonitorInSOTW(
+        'monitorID', monitor.monitorID,
+        {...monitor,
+         token,
+         secret
+        }
+      );
+      io.emit("new token supplied", sotw);
     }
-    monitor = updateToken(monitor);
-    sotw = updateMonitor(monitor.monitorID, monitor);
-    io.emit('new token', monitor);
-  })
+  });
 
-  io.emit("hello", { name: "Zach", adjective: "cool" });
+  socket.on("presentations requested", () => {
+    socket.emit("presentations supplied", {
+      presentations: getDirectories(opts.baseDir + "/presentations/")
+    });
+  });
 
-  socket.on("get presentations", () => {
-    console.log("Get presentations")
-    socket.emit("presentation list", { presentationsNames: getPresentations() })
-  })
-
-  socket.on("presentation request", ({ presentation , sessionID }) => {
-    console.log("oooooh, presentation", {presentation, sessionID });
-    io.emit("set presentation", {presentation, sessionID });
+  socket.on("presentation selected", ({ presentation, monitorName }) => {
+    sotw = updateMonitorInSOTW('monitorName', monitorName, { presentation })
+    io.emit("monitor presentation updated", sotw);
   });
 });
 
-
 server.listen(opts.port || null);
 console.log(`Listening on ${opts.port} and serving ${opts.baseDir}`);
-
-// socket.on('add monitor', () => {
-//   monitor = newMonitor();
-//   sotw = [...sotw, monitor]
-//   io.emit('monitor added', monitor);
-// })
--- static/prezzie-init.js
+++ static/prezzie-init.js
@@ -1,65 +1,56 @@
-const monitor = sessionStorage.getItem("sessionID")
-const qrcodeURL= `${window.location.origin}/presentations.html?m=${monitor}`
-const {socketID, secret} = JSON.parse(sessionStorage.getItem("token") || '{}')
-window.socketID = socketID
-window.secret = secret
-console.log("INIT ITNIT", {socketID, secret})
+const { monitorName, token, secret} = JSON.parse(sessionStorage.getItem("monitor"));
+const qrcodeURL= `${window.location.origin}/presentations.html?m=${monitorName}`
+const isMonitor = window.location.pathname.includes("presentation_client.html");
+window.socketID = token
+window.secret = isMonitor ? null : secret
+console.log("INIT ITNIT", {monitorName, token, secret, window: window.location});
 
 document.addEventListener("DOMContentLoaded", () => {
-    const URL = window.location.origin
-    const socket = io(URL, {autoConnect: true});
+  const URL = window.location.origin
+  const socket = io(URL, {autoConnect: false});
 
-    if (secret == null) {
-      console.log("QRCODE RENDER")
-      const slideBody = document.querySelector("div.reveal")
-      var qrcode = document.createElement("canvas")
-      qrcode.id = 'qrcode-canvas'
-      QRCode.toCanvas(qrcode, qrcodeURL, {width: 135, height: 135}, (error) => {
-          if (error) {
-              console.log('error rendering qr code', {error, value: qrcodeURL});
-          } else {
-              console.log('qr code success!', {value: qrcodeURL});
-          }
-      })
-      slideBody.appendChild(qrcode)
-    } else {
-        const slideBody = document.querySelector('div.reveal');
-        const button = document.createElement("button");
-        button.style.position = "absolute";
-        button.style.top = "0";
-        button.style.left = "0";
-        button.textContent = "Go Back";
-        button.addEventListener('click', () => window.location.href = qrcodeURL);
-        slideBody.appendChild(button);
-        const controls = document.querySelector('.controls');
-        const slides = document.querySelector('.slides');
-        slides.style.filter = "blur(13px)";
-        controls.style.fontSize = "calc(100vh / 16)";
-        console.log({controls, controlslayout: controls.dataset});
-        controls.dataset.controlsLayout = "edges";
-    }
-
-    socket.on("set presentation", ({ presentation, sessionID }) => {
-        console.log("setting presentation to", {presentation, sessionID})
-        if (monitor !== sessionID) {
-            return
-        } else {
-            const prezzyLink = `${window.location.origin}/presentations/${presentation}/presentation_client.html`
-            window.location.href = prezzyLink;
-        }
-    });
-    socket.on("new token", ({ sessionID, socketID, secret }) => {
-        console.log("new token")
-        console.table({ sessionID, socketID, secret })
-        if (sessionID !== monitor) {
-            console.log("no match", {monitor, sessionID})
-            return
-        } else {
-            console.log("match!", {monitor, sessionID})
-            sessionStorage.setItem("token", JSON.stringify({ socketID }))
-            window.socketID = socketID
-        }
-
-        console.log("what happening", socket.auth)
+  if (isMonitor) {
+    let monitor = JSON.parse(sessionStorage.getItem("monitor"));
+    if (!monitor) console.error('on monitor presentation without monitor set, you want to resync');
+    socket.auth = { monitor };
+    socket.connect();
+    const slideBody = document.querySelector("div.reveal")
+    var qrcode = document.createElement("canvas")
+    qrcode.id = 'qrcode-canvas'
+    QRCode.toCanvas(qrcode, qrcodeURL, {width: 135, height: 135}, (error) => {
+      if (error) {
+        console.log('error rendering qr code', {error, value: qrcodeURL});
+      } else {
+        console.log('qr code success!', {value: qrcodeURL});
+      }
     })
+    slideBody.appendChild(qrcode)
+  } else {
+    socket.connect();
+    const slideBody = document.querySelector('div.reveal');
+    const button = document.createElement("button");
+    button.style.position = "absolute";
+    button.style.top = "0";
+    button.style.left = "0";
+    button.textContent = "Go Back";
+    button.addEventListener('click', () => window.location.href = qrcodeURL);
+    slideBody.appendChild(button);
+    const controls = document.querySelector('.controls');
+    const slides = document.querySelector('.slides');
+    slides.style.filter = "blur(13px)";
+    controls.style.fontSize = "calc(100vh / 16)";
+    console.log({controls, controlslayout: controls.dataset});
+    controls.dataset.controlsLayout = "edges";
+  }
+
+  socket.on("new token supplied", (sotw) => {
+    const monitorState = sotw.find(m=> m.monitorName === monitorName);
+    sessionStorage.setItem('monitor', JSON.stringify(monitorState));
+  });
+
+  socket.on("monitor presentation updated", (sotw) => {
+    const monitorState = sotw.find(m=> m.monitorName === monitorName);
+    sessionStorage.setItem('monitor', JSON.stringify(monitorState));
+    window.location.href = `${window.location.origin}/presentations/${monitorState.presentation}/presentation_client.html`
+  });
 })
